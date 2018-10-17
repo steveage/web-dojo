01f38c527fc104a9ef6d12f28bc46d8f25977103
Bialobrzeski,Mariusz <MBialobrzeski@follett.com>
2018-10-17 13:21:05 -0500

    Changed to use deserialize to object only. Replaced TraceWriter with ILogger.

--- HighbushFunctionApp/Function1.cs
+++ HighbushFunctionApp/Function1.cs
@@ -5,26 +5,40 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Build.Framework;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace HighbushFunctionApp
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            IActionResult result;
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            //string name = req.Query["name"];
 
             string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            IotEvent data = JsonConvert.DeserializeObject<IotEvent>(requestBody);
+            //name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            if (data == null)
+            {
+                log.LogTrace("IOT event not provided.");
+                result = new BadRequestObjectResult("Please pass IOT event in the request body.");
+            }
+            else
+            {
+                string message = $"IOT event {data.Event} triggered for {data.Device} device.";
+                log.LogDebug(message);
+                result = new OkObjectResult(message);
+            }
+
+            return result;
         }
     }
 }
---
+++ HighbushFunctionApp/IotEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HighbushFunctionApp
+{
+    public class IotEvent
+    {
+        public string Device { get; set; }
+        public string Event { get; set; }
+    }
+}
