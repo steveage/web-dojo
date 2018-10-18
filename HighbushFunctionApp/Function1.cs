d00b5abe28445b26db035ed333c97026a771e6da
Bialobrzeski,Mariusz <MBialobrzeski@follett.com>
2018-10-17 23:19:49 -0500

    Added storage queue.

--- HighbushFunctionApp/Function1.cs
+++ HighbushFunctionApp/Function1.cs
@@ -1,21 +1,18 @@
-
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Build.Framework;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Logging;
-using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace HighbushFunctionApp
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, [Queue("highbush-automation-queue", Connection = "AzureWebJobsStorage1")] ICollector<IotEvent> queueCollector, ILogger log)
         {
             IActionResult result;
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -28,13 +25,14 @@ namespace HighbushFunctionApp
 
             if (data == null)
             {
-                log.LogTrace("IOT event not provided.");
+                log.LogInformation("IOT event not provided.");
                 result = new BadRequestObjectResult("Please pass IOT event in the request body.");
             }
             else
             {
                 string message = $"IOT event {data.Event} triggered for {data.Device} device.";
-                log.LogDebug(message);
+                queueCollector.Add(data);
+                log.LogInformation(message);
                 result = new OkObjectResult(message);
             }
 
